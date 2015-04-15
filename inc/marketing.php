bd530b9c4d2d747c7ad7c70db3f398fe75f06579
MrRedbeardman <travismstewart@live.com>
2015-04-14 23:32:52 -0400

    fixed marketing.php. JQ gallery needs work on mobile.

--- inc/marketing.php
+++ inc/marketing.php
@@ -7,19 +7,19 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <a href="gallery.php#main-content"><img class="img-circle" src="img/logos/htmlcapture.JPG" alt="Generic placeholder image" width="140" height="140"></a>
+          <a href="gallery.php#main-content"><img class="img-circle" src="img/logos/htmlcapture.jpg" alt="Photo Gallery" width="140" height="140"></a>
           <h2>Photo Gallery</h2>
           <p>Photos from my travels for both work and play.</p>
           <p><a class="btn btn-default" href="gallery.php" role="button">View Gallery &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <a href="about.php#main-content"><img class="img-circle" src="img/logos/profile.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+          <a href="about.php#main-content"><img class="img-circle" src="img/logos/profile.jpg" alt="About Me" width="140" height="140"></a>
           <h2>About Me</h2>
           <p>I prefer the road less traveled... and it has taken me some amazing places.</p>
           <p><a class="btn btn-default" href="about.php#main-content" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <a href="development.php#main-content"><img class="img-circle" src="img/logos/codecapture.JPG" alt="Adventure Blog" width="140" height="140"></a>
+          <a href="development.php#main-content"><img class="img-circle" src="img/logos/codecapture.JPG" alt="Web Development" width="140" height="140"></a>
           <h2>Web Development</h2>
           <p>Need a website for your small business? I'm looking to expand my portfolio. Let's work together.</p>
           <p><a class="btn btn-default" href="development.php#main-content" role="button">View details &raquo;</a></p>
