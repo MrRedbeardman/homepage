af0b7e66877d4a5c1831ed27fb2bcdd41229ac35
MrRedbeardman <travismstewart@live.com>
2015-04-17 17:07:20 -0400

    added meta capture img

--- inc/header.php
+++ inc/header.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
+    <meta property="og:image" content="http://www.travismstewart.com/img/pics/metacapture.jpg"/>  
+    <meta name="description" content="Travis M Stewart. Photography, Web Design, Digital Marketing">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
     <link href='http://fonts.googleapis.com/css?family=Lato:400,400italic|Oswald' rel='stylesheet' type='text/css'>  
-    <title>Travis M. Stewart | </title>
+    <title>| Travis M. Stewart | </title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
